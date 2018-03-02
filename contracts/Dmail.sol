275c3128ee5b1aa7f31ee0f1070dfa9ab29d2d98
Donald <dwkwan@ucsd.edu>
2018-03-02 00:12:59 +0000

    amended Dmail.sol to use string

--- contracts/Dmail.sol
+++ contracts/Dmail.sol
@@ -2,23 +2,24 @@ pragma solidity ^0.4.19;
 
 contract Dmail {
     // exact size in bytes of pem file is 437
-    mapping(address => bytes) public keyStore;
-    mapping(address => bytes) public mailBoxes;
+    mapping(address => string) public keyStore;
+    mapping(address => string) public mailBoxes;
     
     //store_key(caller_address,caller_public_key) will store the caller’s public key as a value in a hashmap with the caller’s address as the key.
-    function storeKey(bytes publicKey) returns (bool success) {
+    function storeKey(string publicKey) returns (bool success) {
+    	//TODO: Add check to see if public key length is 437
         keyStore[msg.sender] = publicKey;
         if (keyStore[msg.sender].length == publicKey.length){
             return keyStore[msg.sender];
         }
     }
     //send_mail(receiver_address,ciphertext,sender_address) will store ciphertext concatenated with sender_address as a value in a hashmap with the receiver_address as the key.
-    function sendMail(address receiverAddress, bytes message) returns (bool success) {
+    function sendMail(address receiverAddress, string message) returns (bool success) {
         mailBoxes[receiverAddress] = message;
         if (mailBoxes[receiverAddress].length ==  message.length){
             return mailBoxes[receiverAddress];
         }
-        //TODO: append msg.sender as bytes
+        //TODO: append msg.sender as string
         //+string(msg.sender);
         
     }
