70987efebf50a5e82cb68275c7aeadba1825b4a5
David Hacker <dmhacker.cs@gmail.com>
2018-03-24 13:05:39 -0700

    Use express to serve mailbox & index files statically

---
+++ index.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var path = require('path');
+
+// Create a new Express application
+var app = express();
+
+// Use the 'build' directory as the public static folder
+app.use(express.static(path.join(__dirname, 'build')));
+
+// Configure routes to omit '.html' suffixes
+app.get('/', function(request, response) {
+  response.sendFile('index.html');
+});
+app.get('/mailbox', function(request, response) {
+  response.sendFile('mailbox/index.html');
+});
+
+// Set port & listen for incoming HTTP requests
+app.set('port', (process.env.PORT || 5000));
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
