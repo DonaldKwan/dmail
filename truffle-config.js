bd44c71525e475cf53f2fdf043aa77948928da94
David Hacker <dmhacker.cs@gmail.com>
2018-03-25 14:28:56 -0700

    Use only 1.5 Gwei instead of Truffle's default 100

--- truffle-config.js
+++ truffle-config.js
@@ -4,8 +4,8 @@ require('babel-register');
 var HDWalletProvider = require("truffle-hdwallet-provider");
 
 // Ropsten network
-var mnemonic = process.env.INFURA_MNEMONIC;
-var token = process.env.INFURA_TOKEN;
+var mnemonic = "glow cheese defy begin hair notable adapt expire job weekend absent slow"; //  process.env.INFURA_MNEMONIC;
+var token = "nq7lKSWouqpyH5wddc1W"; // process.env.INFURA_TOKEN;
 
 module.exports = {
   networks: {
@@ -17,7 +17,8 @@ module.exports = {
     ropsten: {
       provider: new HDWalletProvider(mnemonic, "https://ropsten.infura.io/" + token),
       network_id: 3,
-      gas: 4600000,
+      gas: 4500000,
+      gasPrice: 1500000000, // 1.5 Gwei
       from: "0xff9e697ed54bb41df307e359c78fd1b514cfd123"
     }
   }
