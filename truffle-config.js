f5b77460bac617dcd44e0d0054591ab3f301ac84
David Hacker <dmhacker.cs@gmail.com>
2018-03-24 18:27:22 -0700

    Add ropsten network migration

--- truffle-config.js
+++ truffle-config.js
@@ -1,5 +1,11 @@
 // Allows us to use ES6 in our migrations and tests.
-require('babel-register')
+require('babel-register');
+
+var HDWalletProvider = require("truffle-hdwallet-provider");
+
+// Ropsten network
+var mnemonic = process.env.INFURA_MNEMONIC;
+var token = process.env.INFURA_TOKEN;
 
 module.exports = {
   networks: {
@@ -7,6 +13,10 @@ module.exports = {
       host: '127.0.0.1',
       port: 8545,
       network_id: '*' // Match any network id
+    },
+    ropsten: {
+      provider: new HDWalletProvider(mnemonic, "https://ropsten.infura.io/" + token),
+      network_id: 3
     }
   }
 }
